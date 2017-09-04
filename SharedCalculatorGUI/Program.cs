8ec02bc63df130ca765a1ec3780374b3310c7904
Anne Sofie Molsted Wanscher <anne55x9@edu.easj.dk>
2017-09-04 15:20:26 +0200

    Bruger to variable operation og result med datatyper int og double. Convertere string til double og bruger if else til alle operationerner (ialt fire). Dertil bruges en switch til at definerer case 1-4 operation med tilhørende funktioner fra library,

--- SharedCalculatorGUI/Program.cs
+++ SharedCalculatorGUI/Program.cs
@@ -10,6 +10,81 @@ namespace SharedCalculatorGUI
     {
         static void Main(string[] args)
         {
+            var cal = new SharedCalculator.Lommeregner();
+
+            //Console.WriteLine("Indtast en værdi:");
+
+            
+
+            ////Console.WriteLine(cal.addition(2,3));
+
+            //Console.ReadLine();
+
+
+            //use for the string operation choice
+            int operation = 0;
+            //stock the result of the operation
+            double result = 0;
+
+            //Ask user first number
+            Console.WriteLine("Type you first number :");
+            string stringFirstNumber = Console.ReadLine();
+            double firstNumber = Convert.ToDouble(stringFirstNumber);
+
+            //Ask user second number
+            Console.WriteLine("Type you second number :");
+            string stringSecondNumber = Console.ReadLine();
+            double secondNumber = Convert.ToDouble(stringSecondNumber);
+
+            //Ask user operation to use
+            Console.WriteLine("Enter the operation + (addition), - (subtraction), * (multiplication), / (division), ^ (exposant) or % (reste) :");
+            string stringOperation = Console.ReadLine();
+
+            // Convert string choice to integral
+            if (stringOperation == "+" || stringOperation == "addition")
+            {
+                operation = 1;
+            }
+            else if (stringOperation == "-" || stringOperation == "subtraction")
+            {
+                operation = 2;
+            }
+            else if (stringOperation == "*" || stringOperation == "multiplication")
+            {
+                operation = 3;
+            }
+            else if (stringOperation == "/" || stringOperation == "division")
+            {
+                operation = 4;
+            }
+        
+
+            switch (operation)
+            {
+                case 1:
+                    result = cal.addition(firstNumber, secondNumber);
+                    break;
+
+                case 2:
+                    result = cal.subtraction(firstNumber, secondNumber);
+                    break;
+
+                case 3:
+                    result = cal.multiplication(firstNumber, secondNumber);
+                    break;
+
+                case 4:
+                    result = cal.division(firstNumber, secondNumber);
+                    break;
+            }
+
+           
+            Console.WriteLine("\nResult of " + firstNumber + " " + stringOperation + " " + secondNumber + " = " + result + ".");
+
+            
+            Console.ReadKey();
+
+
         }
     }
 }
